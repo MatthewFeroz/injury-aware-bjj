ff82077bfe35bd1817b786875ae7d2746079769f
MatthewFeroz <matthewferoz@gmail.com>
2025-10-07 22:01:55 -0400

    refactor: Conform to standard build output for Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,18 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: path.resolve(__dirname, '../static/frontend'),
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/app.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name][extname]',
-      }
-    }
-  }
+  plugins: [react()]
 })
 
 
